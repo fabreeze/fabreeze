417fd5bde4866a50f6429091bfccb755585a63a3
fabreeze <fabreezie@gmail.com>
2014-12-08 22:34:20 -0600

    test committ 2

---
+++ journal.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
+# # Journal 
+# 
+# by /u/fabreeze <br>
+# created on Dec 8, 2014
+
+# <markdowncell>
+
+# ##### Info about version control 
+# 
+# [git syntax tutorial](http://nbviewer.ipython.org/github/demotu/BMC/blob/master/notebooks/VersionControlGitGitHub.ipynb)
+
+# <codecell>
+
+#### Entry 1
+!date
+
+# <codecell>
+
+%%bash
+git add journal.ipynb
+git commit
+
+# <codecell>
+
+
