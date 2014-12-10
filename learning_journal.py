9e739ad8df97e45926c9b9a92a94bf2bf2b9f33f
fabreeze <fabreezie@gmail.com>
2014-12-09 18:52:35 -0600

    saving

---
+++ learning_journal.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
+# # Journal 
+# 
+# by /u/fabreeze <br>
+# created on Dec 8, 2014
+
+# <markdowncell>
+
+# ##### Info about version control 
+# 
+# [git syntax tutorial](http://nbviewer.ipython.org/github/demotu/BMC/blob/master/notebooks/VersionControlGitGitHub.ipynb)
+
+# <markdowncell>
+
+# To clone this repo: <br>
+# > git clone http://www.github.com/fabreeze/fabreeze.git
+
+# <markdowncell>
+
+# To install ipython notebook
+# > sudo apt-get install python-pip <br>
+# > pip install "ipython[all]"
+
+# <markdowncell>
+
+# To start an ipython session: 
+# > ipython notebook --script
+
+# <markdowncell>
+
+# #### Useful extensions
+
+# <markdowncell>
+
+# Version Information Extension
+# > %install_ext http://raw.github.com/jrjohansson/version_information/master/version_information.py
+
+# <codecell>
+
+%load_ext version_information
+
+# <codecell>
+
+%version_information
+
+# <markdowncell>
+
+# #### Saving changes to github
+
+# <codecell>
+
+%%bash 
+git commit -a -m "saving notebook"
+git push origin master
+
+# <codecell>
+
+%%bash
+git push origin master
+
+# <codecell>
+
+!date
+
+# <codecell>
+
+%%bash
+git 
+
